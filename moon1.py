00c2cf7ed802a18c059ca19a542afb818fd3dfbb
Vicky Chan <vickychan@Vickys-MacBook-Air.local>
2026-05-21 11:28:52 +0530

    updated

--- moon1.py
+++ moon1.py
@@ -1,3 +1,3 @@
-Moon is the coolest planet 
-Moon is very near to earth
+Moon is the coolest planet in our solar sysytem 
+Moon is very near to earth approx 3,00,000 lakh KM away from earth
 Latest mission to reach moon and humans will leave there very soon
